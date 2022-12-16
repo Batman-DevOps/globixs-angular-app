aafd832a1369d7799320df7cbb79027b851963a6
Satyajeet <satyajeetthakre@gmail.com>
2022-12-16 18:09:23 +0530

    WhatsApp integration

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -8,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   youthMembers = ['Sahasra', 'Amy', 'Anwita', 'Haley', 'Krithika', 'Saisha'];
-  
+
+  logos: any[] = [
+    { name: 'Adidas', logo: 'assets/images/logos/adidas.jpg' },
+    { name: 'Amazon', logo: 'assets/images/logos/amazon.jpg' },
+    { name: 'Flipkart', logo: 'assets/images/logos/flipkart.jpg' },
+    { name: 'H&M', logo: 'assets/images/logos/h&m.jpg' },
+    { name: 'Myntra', logo: 'assets/images/logos/myntra.jpg' },
+    { name: 'Nike', logo: 'assets/images/logos/nike.jpg' },
+    { name: 'Adidas', logo: 'assets/images/logos/adidas.jpg' },
+    { name: 'Amazon', logo: 'assets/images/logos/amazon.jpg' },
+    { name: 'Flipkart', logo: 'assets/images/logos/flipkart.jpg' },
+    { name: 'H&M', logo: 'assets/images/logos/h&m.jpg' },
+    { name: 'Myntra', logo: 'assets/images/logos/myntra.jpg' },
+    { name: 'Nike', logo: 'assets/images/logos/nike.jpg' },
+    { name: 'Adidas', logo: 'assets/images/logos/adidas.jpg' },
+    { name: 'Amazon', logo: 'assets/images/logos/amazon.jpg' },
+    { name: 'Flipkart', logo: 'assets/images/logos/flipkart.jpg' },
+    { name: 'H&M', logo: 'assets/images/logos/h&m.jpg' },
+    { name: 'Myntra', logo: 'assets/images/logos/myntra.jpg' },
+    { name: 'Nike', logo: 'assets/images/logos/nike.jpg' }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
