fce95802962155e985baa47d504052db9ef55e72
Batman-DevOps <satyajeetthakre@gmail.com>
2022-12-29 19:52:05 +0530

    CSS changes for sliding client info

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -21,6 +21,25 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var sections = document.querySelectorAll('section');
+    var options = {
+      rootMargin: '0px',
+      threshold: 0.25
+    }
+    var callback = (entries: any) => {
+      entries.forEach((entry: any) => {
+        var target = entry.target;
+        if (entry.intersectionRatio >= 0.25) {
+          target.classList.add("is-inview");
+        } else {
+          target.classList.remove("is-inview");
+        }
+      })
+    }
+    var observer = new IntersectionObserver(callback, options)
+    sections.forEach((section, index) => {
+      observer.observe(section)
+    })
   }
 
 }
